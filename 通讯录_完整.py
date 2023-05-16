07d53d653c6547b00c2de386f4cc1a88924c0cf3
luu <1185445667@qq.com>
2023-05-16 22:55:16 +0800

    更新部分注释

--- 通讯录_完整.py
+++ 通讯录_完整.py
@@ -244,6 +244,7 @@ class ContactManager:
             rows = [row for row in reader if
                     (keyword in row[1]) or (keyword in row[2])]
 
+
         # 显示查找结果
         if not rows:
             messagebox.showinfo('提示', '未找到符合条件的记录！')
